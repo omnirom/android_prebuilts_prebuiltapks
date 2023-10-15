cff0de3cf022d0f02d114fa2fc7e0b3d415d75e8
maxwen <max.weninger@gmail.com>
2023-10-15 20:15:26 +0200

    move AuroraStore out of priv-app
    there is no real need anymore and will make our
    live so much easier like eg enabling in-app updates
    
    this is possible now since session installer is
    a user permission available to any app
    
    Change-Id: Ic7ccb15b3279d32c79f6f0589ac0c842d4333248

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -1,23 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-aurorastore.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := AuroraStore
 LOCAL_SRC_FILES := AuroraStore_4.2.3.apk
 LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
-LOCAL_REQUIRED_MODULES := privapp-permissions-aurorastore.xml
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions \
     androidx.window.sidecar
 include $(BUILD_PREBUILT)
