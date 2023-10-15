d1c9ebc6c7cc79484e27606af0acb5c714955307
maxwen <max.weninger@gmail.com>
2023-10-15 20:16:25 +0200

    Update AuroraStore to 4.3.3
    Change-Id: I5d58339558d06e04c16a082eeece0d30934f1f41

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := AuroraStore
-LOCAL_SRC_FILES := AuroraStore_4.2.3.apk
+LOCAL_SRC_FILES := AuroraStore_4.3.3.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_DEX_PREOPT := false
