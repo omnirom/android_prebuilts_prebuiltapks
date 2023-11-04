78cf2f884b982a00f318916f593cb508ecaf9133
maxwen <max.weninger@gmail.com>
2023-11-04 01:35:10 +0100

    microg: unifiednlp backends are obsolete
    location stack is done in gmscore now
    
    Change-Id: Id9e7f7704835afcdf6bcf3ea767c096194ef344e

--- microg.mk
+++ microg.mk
@@ -1,12 +1,8 @@
 # Product priv-app
 PRODUCT_PACKAGES += \
     AuroraStore \
-    com.google.android.maps.xml \
-    com.google.android.maps.jar \
     FakeStore \
     GmsCore \
     GsfProxy \
-    microg-a5k.xml \
-    MozillaNlpBackend \
-    NominatimNlpBackend
+    microg-a5k.xml
 
